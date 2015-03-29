15441dbe7b9c64ab713917ff13c1a3307fb21ffb
Angblah <AngeloBMarcelo@gmail.com>
2015-03-29 18:58:07 -0400

    LU Factorization Updated

--- part1/lu_fact.m
+++ part1/lu_fact.m
@@ -1,5 +1,18 @@
 function [L,U] = lu_fact(A)
 
-    
+    [numRows,numCol] = size(A);
+    j = 1;
+    i = 2;
+    L = eye(size(A));
+    U = A;
+    while j < numCol
+        i = j+1;
+        while i > j & i <= numRows
+            L(i,j) = U(i,j)/U(j,j)
+            U(i,:) = U(i,:) - L(i,j) * U(j,:)
+            i = i+1;
+        end
+        j = j+1;
+    end
 
 end
